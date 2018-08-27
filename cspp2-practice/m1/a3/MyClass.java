a888e9fb6e7ea55569dc0f37811ba86f5b340850
Abhilash11Addanki <abhilash11addanki@msitprogram.net>
2018-08-27 18:17:55 +0530

    HelloWorld

--- cspp2-practice/m1/a3/MyClass.java
+++ cspp2-practice/m1/a3/MyClass.java
@@ -1,7 +1,20 @@
-public class MyClass 
-{
- public static void main(String[] args)
- {
+/**.
+*The class MyClass displays the HelloWorld
+*/
+public final class MyClass {
+/**.
+*MyClass constructor
+*/
+private MyClass() {
+
+}
+/**.
+*This is the main method
+*/
+/**.
+* @param args args are command line arguments
+*/
+ public static void main(final String[] args) {
   System.out.println("Hello World");
  }
 }
