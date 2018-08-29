175d0c0486bae5dfafbe6d2ced145151c4756827
Abhilash11Addanki <abhilash11addanki@msitprogram.net>
2018-08-29 12:15:28 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

---
+++ cspp2-assignments/m3/Assignment-3/Solution.java
@@ -0,0 +1,40 @@
+
+import java.util.Scanner;
+/**
+ * Class for solution.
+ */
+public final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+
+    }
+/**
+ * main method.
+ * @param      args  The arguments
+ */
+    public static void main(final String[] args) {
+
+        Scanner s=new Scanner(System.in);      
+        int n1 = s.nextInt();
+        int n2 = s.nextInt();
+        int gcd = gcd(n1, n2);
+        System.out.println(gcd);
+    }
+    /**
+     * gcd function.
+     * @param      a     { parameter_description }
+     * @param      b     { parameter_description }
+     * @return     { description_of_the_return_value }
+     */
+    static int gcd(final int a, final int b) {
+        int gcd = 1;
+        for (int i = 1; i <= a && i <= b; i++) {
+            if (a % i == 0 && b % i == 0) {
+                gcd = i;
+            }
+        }
+        return gcd;
+    }
+}
