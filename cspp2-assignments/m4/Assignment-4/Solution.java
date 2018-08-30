d9eb5ca4b0cfebec36e90adb797da313c5d88056
Abhilash11Addanki <abhilash11addanki@msitprogram.net>
2018-08-30 21:38:04 +0530

    "check_style_score -> 5 of 5 passed. style score: 1/1 "

--- cspp2-assignments/m4/Assignment-4/Solution.java
+++ cspp2-assignments/m4/Assignment-4/Solution.java
@@ -14,11 +14,10 @@ public final class Solution {
      * main method.
      * @param      args  The arguments
      */
-    public static void main(final String[] args)
-    {
+    public static void main(final String[] args) {
         Scanner sc = new Scanner(System.in);
         String s = sc.nextLine();
-        String reverse = reverseString(s);  
+        String reverse = reverseString(s);
         System.out.println(reverse);
     }
     /**
@@ -29,7 +28,7 @@ public final class Solution {
     static String reverseString(final String str) {
         String[] s = str.split("");
         String res = "";
-        for (int i = s.length-1; i >= 0; i--) {
+        for (int i = s.length - 1; i >= 0; i--) {
             res += s[i];
         }
         return res;
