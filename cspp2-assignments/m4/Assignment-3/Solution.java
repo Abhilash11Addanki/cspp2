222204f57ae345952f8820159bc517283338a0d8
Abhilash11Addanki <abhilash11addanki@msitprogram.net>
2018-08-30 21:08:19 +0530

    "check_style_score -> 1 of 1 passed. style score: 0/1 "

--- cspp2-assignments/m4/Assignment-3/Solution.java
+++ cspp2-assignments/m4/Assignment-3/Solution.java
@@ -1,34 +1,42 @@
 
-// Given an String, Write a java method that returns the decimal value for the given binary string.
-
 import java.lang.Math;
 import java.util.Scanner;
-public class Solution
-{/*
-	Do not modify this main function.
-	*/
-	public static void main(String[] args)
-	{
-		Scanner sc=new Scanner(System.in);
-		int n= sc.nextInt();
-		for(int i=0;i<n;i++){
-			String s=sc.next();
-			String res=binaryToDecimal(s);//Write binaryToDecimal function
-			System.out.println(res);
-		}
-	}
-	static String binaryToDecimal(String s) {
-		String[] s1 = s.split("");
-		int cnt = s1.length-1;
-		int sum = 0;
-		for (int i = 0; i < s1.length; i++){
-			if (Integer.parseInt(s1[i]) == 1) {
-				sum += Math.pow(2,cnt);
-				cnt -= 1;
-			} else {
-				cnt -= 1;
-			}
-		}
-		return Integer.toString(sum);
-	}
+/**
+ * Class for solution.
+ */
+public final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+
+    }
+    /**
+     * main method.
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 0; i < n; i++){
+            String s = sc.next();
+            String res = binaryToDecimal(s);//Write binaryToDecimal function
+            System.out.println(res);
+        }
+    }
+    static String binaryToDecimal(String s) {
+        String[] s1 = s.split("");
+        int cnt = s1.length-1;
+        int sum = 0;
+        for (int i = 0; i < s1.length; i++){
+            if (Integer.parseInt(s1[i]) == 1) {
+                sum += Math.pow(2,cnt);
+                cnt -= 1;
+            } else {
+                cnt -= 1;
+            }
+        }
+        return Integer.toString(sum);
+    }
 }
