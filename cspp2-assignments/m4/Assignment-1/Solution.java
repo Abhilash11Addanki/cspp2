7275d86e40c98b1babf64e7be91315a0cf841279
Abhilash11Addanki <abhilash11addanki@msitprogram.net>
2018-08-30 18:32:23 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2-assignments/m4/Assignment-1/Solution.java
+++ cspp2-assignments/m4/Assignment-1/Solution.java
@@ -3,8 +3,7 @@ import java.util.Scanner;
 /**
  * Class for solution.
  */
-public final class Solution
-{
+public final class Solution {
     /**
      * Constructs the object.
      */
@@ -15,11 +14,10 @@ public final class Solution
      * main method.
      * @param      args  The arguments
      */
-    public static void main(final String[] args)
-    {
+    public static void main(final String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n= sc.nextInt();
-        int arr[] = new int[n];
+        int n = sc.nextInt();
+        int[] arr = new int[n];
         int max = 0;
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
