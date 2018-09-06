9ed6cb45166b82a2b65240ea48c29123d354eab0
Abhilash11Addanki <abhilash11addanki@msitprogram.net>
2018-09-06 13:59:15 +0530

    "check_style_score -> 3 of 3 passed. style score: 0/1 "

--- cspp2-assignments/m10/Assignment-2/Solution.java
+++ cspp2-assignments/m10/Assignment-2/Solution.java
@@ -3,12 +3,18 @@ import java.util.Scanner;
 /**
  * Class for solution.
  */
-public class Solution {
+public final class Solution {
+	/**
+	 * Constructs the object.
+	 */
+	private Solution() {
+
+	}
     /**
      * { function_description }.
      * @param      args  The arguments
      */
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         // create an object of the list to invoke methods on it
         StringList sl = new StringList();
         StringListInterface l = (StringListInterface)sl;//Typecasting is done for StringListInterface
