f657b10c05bb60c43895d176cce6705688069039
Abhilash11Addanki <abhilash11addanki@msitprogram.net>
2018-09-06 16:25:50 +0530

    "check_style_score -> 6 of 6 passed. style score: 0/1 "

--- cspp2-assignments/m10/Assignment-3/Solution.java
+++ cspp2-assignments/m10/Assignment-3/Solution.java
@@ -8,34 +8,50 @@ class Student {
      * { var_description }.
      */
     private String name;
-    
-    public Student(String name) {//A constructor used to initialize the instance variables
+    /**
+     * Constructs the object.
+     * @param      name  The name
+     */
+    public Student(String name) {
         this.name = name;
     }
-
-    public String getName() {//Getter method which returns the value of instance variable
+    /**
+     * Gets the name.
+     * @return     The name.
+     */
+    public String getName() {
         return this.name;
     }
-
-    @Override
+    /**
+     * { function_description }.
+     * @param      other  The other
+     * @return     { description_of_the_return_value }
+     */
     public boolean equals(Object other) { //This method is to check if two students names are equal or not
         if (!(other instanceof Student)) {
             return false;
         }
-
         Student that = (Student) other;
         return this.getName().equals(that.getName());
     }
-
+    /**
+     * Returns a string representation of the object.
+     * @return     String representation of the object.
+     */
     public String toString() {
         return this.name;
     }
 }
-
+/**
+ * Class for solution.
+ */
 public class Solution {
-    
-
-    public static void main(String[] args) {
+    /**
+     * { function_description }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
         // create an object of the list to invoke methods on it
         Scanner stdin = new Scanner(new BufferedInputStream(System.in));
         String objectType = stdin.nextLine();
@@ -54,7 +70,7 @@ public class Solution {
                             listString.add(tokens[1]);
                         break;
                         case "addAll":
-                        if(tokens.length==2){
+                        if(tokens.length == 2){
                         String[] t1 = tokens[1].split(",");
                         listString.addAll(t1);
                         }
@@ -84,11 +100,13 @@ public class Solution {
                         case "contains":
                         System.out.println(listString.contains(tokens[1]));
                         break;
+                        default:
+                        break;
                     }
                 }
             break;
 
-            case "I"://This case will be executed for Integer type list
+            case "I":
                 List<Integer> listInteger = new List();
                 while (stdin.hasNext()) {
                     // read the line
@@ -101,10 +119,10 @@ public class Solution {
                             listInteger.add(Integer.parseInt(tokens[1]));
                         break;
                         case "addAll":
-                        if(tokens.length==2){
+                        if(tokens.length == 2){
                         String[] t1 = tokens[1].split(",");
                         Integer[] temp = new Integer[t1.length];
-                        for (int i = 0;i < temp.length; i++ ) {
+                        for (int i = 0;i < temp.length; i++) {
                             temp[i] = Integer.parseInt(t1[i]);
                         }
                         listInteger.addAll(temp);
@@ -139,7 +157,7 @@ public class Solution {
                 }
             break;
 
-            case "F"://This case will be executed for Float type list
+            case "F":
                 List<Float> listFloat = new List();
                 while (stdin.hasNext()) {
                     // read the line
@@ -152,10 +170,10 @@ public class Solution {
                             listFloat.add(Float.parseFloat(tokens[1]));
                         break;
                         case "addAll":
-                        if(tokens.length==2){
+                        if(tokens.length == 2){
                         String[] t1 = tokens[1].split(",");
                         Float temp[] = new Float[t1.length];
-                        for (int i = 0; i < t1.length ;i++ ) {
+                        for (int i = 0; i < t1.length ; i++) {
                             temp[i] = Float.parseFloat(t1[i]);
                         }
                         listFloat.addAll(temp);
@@ -203,10 +221,10 @@ public class Solution {
                             listCharacter.add(tokens[1].charAt(0));
                         break;
                         case "addAll":
-                        if(tokens.length==2){
+                        if(tokens.length == 2){
                         String[] t1 = tokens[1].split(",");
                         Character[] temp = new Character[t1.length];
-                        for (int i = 0; i < t1.length ;i++ ) {
+                        for (int i = 0; i < t1.length ; i++) {
                             temp[i] = t1[i].charAt(0);
                         }
                         listCharacter.addAll(temp);
@@ -254,10 +272,10 @@ public class Solution {
                             listDouble.add(Double.parseDouble(tokens[1]));
                         break;
                         case "addAll":
-                        if(tokens.length==2){
+                        if(tokens.length == 2){
                         String[] t1 = tokens[1].split(",");
                         Double[] temp = new Double[t1.length];
-                        for (int i = 0; i < t1.length ;i++ ) {
+                        for (int i = 0; i < t1.length ; i++) {
                             temp[i] = Double.parseDouble(t1[i]);
                         }
                         listDouble.addAll(temp);
@@ -305,10 +323,10 @@ public class Solution {
                             listStudent.add(new Student(tokens[1]));
                         break;
                         case "addAll":
-                        if(tokens.length==2){
+                        if(tokens.length == 2){
                         String[] t1 = tokens[1].split(",");
                         Student[] temp = new Student[t1.length];
-                        for (int i = 0; i < t1.length ;i++ ) {
+                        for (int i = 0; i < t1.length ; i++) {
                             temp[i] = new Student(t1[i]);
                         }
                         listStudent.addAll(temp);
