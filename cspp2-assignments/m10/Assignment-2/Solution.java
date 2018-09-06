5ab01bc2d1264803c156ed11828e0be20ee007e1
Abhilash11Addanki <abhilash11addanki@msitprogram.net>
2018-09-06 14:02:14 +0530

    "check_style_score -> 3 of 3 passed. style score: 0/1 "

--- cspp2-assignments/m10/Assignment-2/Solution.java
+++ cspp2-assignments/m10/Assignment-2/Solution.java
@@ -4,12 +4,12 @@ import java.util.Scanner;
  * Class for solution.
  */
 public final class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
 
-	}
+    }
     /**
      * { function_description }.
      * @param      args  The arguments
