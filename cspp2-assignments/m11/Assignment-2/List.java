2a74cee9fbd9f20eb25fe353d85ad7efcf2444d9
Abhilash11Addanki <abhilash11addanki@msitprogram.net>
2018-09-07 17:17:28 +0530

    "check_style_score -> 8 of 8 passed. style score: 0/1 "

--- cspp2-assignments/m11/Assignment-2/List.java
+++ cspp2-assignments/m11/Assignment-2/List.java
@@ -1,44 +1,46 @@
 import java.util.Arrays;
-/*
-     * The goal for the list is to store items.
-     * How are we going to store the items in the list?
-     * An array would be good. Right?
-     * So, when we do not what we are going to have in the list
-     * We need to create a Generic list to store the items
-
-     * Here E is a type parameter, and it will be replaced with
-        actual type when the object got created.
-     */
+/**
+ * List class.
+ * @param      <E>   { parameter_description }
+ */
 public class List<E> {
+	/**
+	 * { var_description }.
+	 */
     private E[] list;
+    /**
+     * { var_description }.
+     */
     private int size;
-    //Constructor
+    /**
+     * Constructs the object.
+     */
     public List() {
-        // Create a variable of the type Object[]
-        list = ((E[])new Object[10]);//Object is the base class for all the classes
+        list = ((E[])new Object[10]);
         size = 0;
     }
-    //Overloaded Constructor
-    public List(int n) {
+    /**
+     * Constructs the object.
+     * @param      n     { parameter_description }
+     */
+    public List(final int n) {
         list = ((E[])new Object[n]);
         size = 0;
     }
-    /* The add method does what the name suggests.
-     * Add a generic type item to the list.
-     * The assumption is to store the item at the end of the list
-     * What is the end of the list?
-     * Is it the same as the end of the array?
-     * Think about how you can use the size variable to add item
-     * to the list.
+    /**
+     * { function_description }.
+     * @param      item  The item
      */
-    public void add(E item) {
+    public void add(final E item) {
         //Inserts the specified element at the end of the list.
         //You can modify the code in this method.
         list[size++] = item;
     }
-    /*Inserts all the elements of specified int
-    array to the end of list*/
-    public void addAll(E[] items) {
+    /**
+     * Adds all.
+     * @param      items  The items
+     */
+    public void addAll(final E[] items) {
         //Write logic for addAll method
         int newsize = size;
         for (int i = newsize, j = 0; i < newsize + items.length; i++) {
@@ -47,44 +49,25 @@ public class List<E> {
         }
     }
     /**
-     * { function_description }
+     * { function_description }.
      */
     private void resize() {
     	int n = 2 * size;
         E[] newarr = java.util.Arrays.copyOf(list, n);
     }
 
-    /*
-     * The size method returns the value of the size.
-     * The purpose of the method is to announce the size of the list
-     * to the objects outside the list
-     *
-     * The method returns an int. Empty list should return 0.
+    /**
+     * { function_description }.
+     * @return     { description_of_the_return_value }
      */
     public int size() {
     	return size;
     }
-    /*
-     * The remove method does what the name suggests.
-     * Removes a String item, specified by the index argument, from the list
-     * It also does an additional step.
-     * Think about what happens when
-     * an item is removed from the middle of the list
-     * It creates a hole in the list, right?
-     * This would mean, all the items that are
-     * to the right side of the removed item should be
-     * moved to the left by one position.
-     * Here is an example:
-     * array = [1,2,3,0,0,0,0,0,0,0]
-     * remove(1) would remove the item 2 which is at index position 1.
-     * But how do you remove the item from an array?
-     * Well, the way to remove it is to move all
-     * the items, that are to the right of the removed item, to the left
-     * So, the new array looks like this.
-     * array = [1,3,0,0,0,0,0,0,0,0]
-     * The method returns void (nothing)
+    /**
+     * { function_description }.
+     * @param      index  The index
      */
-    public void remove(int index) {
+    public void remove(final int index) {
         //Write logic for remove method
         if (index >= 0 && index < size) {
             for (int i = index; i < size - 1; i++) {
@@ -95,43 +78,24 @@ public class List<E> {
             System.out.println("Invalid Position Exception");
         }
     }
-    /*
-     * Get method has to return the items that is
-     * at the index position passed as an argument to the method.
-     * If the item doesn't exist then return a -1 to indicate that
-     * there is no element at that index.
-     * How can an element not be there at a given position?
-     * Well, if the position is greater than the number of items
-     * in the list then that would mean the item doesn't exist.
-     * How do we check if the position is greater than the
-     * number of items in the list? Would size variable be useful?
+    /**
+     * { function_description }.
+     *
+     * @param      index  The index
+     *
+     * @return     { description_of_the_return_value }
      */
-    public E get(int index) {
+    public E get(final int index) {
          //Write logic for get method
     	if (index < 0 || index >= size) {
     		return null;
     	}
     	return list[index];
     }
-    /*
-     * What happens when you print an object using println?
-     * Java provides a method named toString that is internally
-     * invoked when an object variable is used in println.
-     * For example:
-     * List l = new List();
-     * System.out.println(l);
-     * This statement is a shortcut for
-     * System.out.println(l.toString());
+    /**
+     * Returns a string representation of the object.
      *
-     * So, implement the toString method to display the items
-     * in the list in the square brackets notation.
-     * i.e., if the list has numbers 1, 2, 3
-     * return the string [1,2,3]
-     * Caution: The array may be having other elements
-     * Example: [1,2,3,0,0,0,0,0,0,0]
-     * toString should only return the items in the list and
-     * not all the elements of the array.
-     *
+     * @return     String representation of the object.
      */
     public String toString() {
     	E[] temparr = ((E[])new Object[size]);
@@ -140,13 +104,14 @@ public class List<E> {
         }
        return Arrays.toString(temparr).replace(" ", "");
     }
-    /*
-     * Contains return true if the list has
-     * the item passed as an argument to the method
-     * So, iterate through the list and return true if
-     * the item exists and otherwise false
+    /**
+     * { function_description }.
+     *
+     * @param      item  The item
+     *
+     * @return     { description_of_the_return_value }
      */
-    public boolean contains(E item) {
+    public boolean contains(final E item) {
 		//Write logic for contains method
 		for (int i = 0; i < size; i++) {
 			if (list[i].equals(item)) {
@@ -155,13 +120,14 @@ public class List<E> {
 		}
         return false;
     }
-    /*
-     * Returns the index of the first occurrence
-     * of the specified element in this list,
-     * or -1 if this list does not contain the element.
+    /**
+     * Searches for the first match.
+     *
+     * @param      item  The item
+     *
+     * @return     { description_of_the_return_value }
      */
-
-    public int indexOf(E item) {
+    public int indexOf(final E item) {
        //Write logic for indexOf method
     	for (int i = 0; i < size; i++) {
     		if (list[i] == item) {
@@ -171,10 +137,12 @@ public class List<E> {
         return -1;
     }
 
-    /* Removes all of its elements that
-     * are contained in the specified int array.
+    /**
+     * Removes all.
+     *
+     * @param      items  The items
      */
-    public void removeAll(E[] items)
+    public void removeAll(final E[] items)
     {
         // write the logic
         for (int i = 0; i < items.length; i++) {
@@ -187,12 +155,15 @@ public class List<E> {
         }
     }
 
-    /*Returns a list containing elements, including
-     startIndex and excluding endIndex. The first parameter
-     indicates the startIndex and the second parameter
-     indicates the endIndex.
+    /**
+     * { function_description }.
+     *
+     * @param      n     { parameter_description }
+     * @param      n2    The n 2
+     *
+     * @return     { description_of_the_return_value }
      */
-    public List subList(int n, int n2) {
+    public List subList(final int n, final int n2) {
     	List l = new List();
         if ((n < 0 || n2 < 0) || (n > n2) || size == 0) {
             System.out.println("Index Out of Bounds Exception");
@@ -204,11 +175,14 @@ public class List<E> {
         }
     return l;
     }
-    /*Returns a boolean indicating whether the parameter
-      i.e a List object is exactly matching with the given list or not.
+    /**
+     * { function_description }.
+     *
+     * @param      listdata  The listdata
+     *
+     * @return     { description_of_the_return_value }
      */
-    public boolean equals(List<E> listdata)
-    {
+    public boolean equals(final List<E> listdata) {
         if (size != listdata.size) {
             return false;
         }
@@ -219,7 +193,9 @@ public class List<E> {
         }
     return true;
     }
-    /*Removes all the elements from list*/
+    /**
+     * { function_description }.
+     */
     public void clear()
     {
         // write the logic for clear.
