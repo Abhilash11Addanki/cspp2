e62b1a535581cf0379896d2b28314bab6242eafb
Abhilash11Addanki <abhilash11addanki@msitprogram.net>
2018-09-07 14:18:49 +0530

    "check_style_score -> 3 of 3 passed. style score: 1/1 "

--- cspp2-assignments/m11/Assignment-1/List.java
+++ cspp2-assignments/m11/Assignment-1/List.java
@@ -305,7 +305,7 @@ public class List {
                     if (tokens.length == 2) {
                         String[] lt = tokens[1].split(",");
                         List l2 = new List();
-                        for (int k = 0; k < lt.length; k++ ) {
+                        for (int k = 0; k < lt.length; k++) {
                             l2.add(Integer.parseInt(lt[k]));
                         }
                         System.out.println(l.equals(l2));
@@ -320,3 +320,5 @@ public class List {
         }
     }
 }
+
+
