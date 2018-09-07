62f3ddbd37f73582c20b94f89ac92a5e26f149c1
Abhilash11Addanki <abhilash11addanki@msitprogram.net>
2018-09-07 13:56:57 +0530

    "check_style_score -> 2 of 3 passed. style score: 0/1 "

--- cspp2-assignments/m11/Assignment-1/List.java
+++ cspp2-assignments/m11/Assignment-1/List.java
@@ -157,10 +157,10 @@ public class List {
      public void removeAll(int[] newArray) {
         // write the logic
         int[] removearr = Arrays.copyOf(arr, arr.length);
-        for (int i = 0; i < newArray.length; i++) {
-        	for (int j = 0; j < removearr.length; j++) {
-        		if (removearr[j] == newArray[i]) {
-        			remove(j);
+        for (int i = 0; i < removearr.length; i++) {
+        	for (int j = 0; j < newArray.length; j++) {
+        		if (removearr[i] == newArray[j]) {
+        			remove(i);
         		}
         	}
         }
