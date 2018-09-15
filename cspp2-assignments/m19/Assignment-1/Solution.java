1310641f76897965b24414fec5b7f5e647b2e367
Abhilash11Addanki <abhilash11addanki@msitprogram.net>
2018-09-15 11:49:59 +0530

    "check_style_score -> 5 of 13 passed. style score: 0/1 "

--- cspp2-assignments/m19/Assignment-1/Solution.java
+++ cspp2-assignments/m19/Assignment-1/Solution.java
@@ -77,6 +77,10 @@ public final class Solution {
 			System.out.println(field[0] + " " + "does not have enough answer choices");
 			return;
 		}
+		if (Integer.parseInt(field[2]) < 1 || Integer.parseInt(field[2]) >= choices.length) {
+			System.out.println("Error! Correct answer choice number is out of range for" + " " + field[0]);
+			return;
+		}
 		if (Integer.parseInt(field[3]) <= 0) {
 			System.out.println("Invalid max marks for" + " " + field[0]);
 			return;
