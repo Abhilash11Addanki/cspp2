7a115b7886229b0442686374f47cdfe2a3d69968
Abhilash11Addanki <abhilash11addanki@msitprogram.net>
2018-09-15 13:11:48 +0530

    "check_style_score -> 7 of 13 passed. style score: 1/1 "

--- cspp2-assignments/m19/Assignment-1/Solution.java
+++ cspp2-assignments/m19/Assignment-1/Solution.java
@@ -122,7 +122,7 @@ public final class Solution {
     public static void startQuiz(final Scanner s,
         final Quiz quiz, final int answerCount) {
         for (int i = 0; i < answerCount; i++) {
-            System.out.println(quiz.getquestiontext());
+            quiz.getquestiontext();
         }
     }
     /**
