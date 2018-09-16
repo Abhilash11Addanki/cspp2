3a9ccd5f3bd8df39440e959d6f12431e9b763195
Abhilash11Addanki <abhilash11addanki@msitprogram.net>
2018-09-16 22:24:33 +0530

    "check_style_score -> 7 of 13 passed. style score: 0/1 "

--- cspp2-assignments/m19/Assignment-1/Solution.java
+++ cspp2-assignments/m19/Assignment-1/Solution.java
@@ -62,7 +62,7 @@ public final class Solution {
      * @param      questionCount  The question count
      */
     public static void loadQuestions(final Scanner s,
-            final Quiz quiz, final int questionCount) {
+                                     final Quiz quiz, final int questionCount) {
         /**
          * questions list.
          */
@@ -83,13 +83,13 @@ public final class Solution {
             String[] choices = field[1].split(",");
             if (choices.length < 2) {
                 System.out.println(field[0] + " "
-                    + "does not have enough answer choices");
+                                   + "does not have enough answer choices");
                 return;
             }
             if (Integer.parseInt(field[2]) < 1
                     || Integer.parseInt(field[2]) > choices.length) {
                 System.out.println("Error! Correct answer choice number"
-                    + " " + "is out of range for" + " " + field[0]);
+                                   + " " + "is out of range for" + " " + field[0]);
                 return;
             }
             final int three = 3;
@@ -115,7 +115,7 @@ public final class Solution {
      * @param      answerCount  The answer count
      */
     public static void startQuiz(final Scanner s,
-            final Quiz quiz, final int answerCount) {
+                                 final Quiz quiz, final int answerCount) {
         quiz.displayquestiontext();
     }
     /**
@@ -193,7 +193,10 @@ class Quiz {
     }
     void displayquestiontext() {
         for (String quiz : questiontext) {
-            System.out.println(quiz);
+            for (int quiz1 : marksawarded) {
+                System.out.println(quiz + "(" + quiz1 + ")");
+                break;
+            }
         }
     }
 }
