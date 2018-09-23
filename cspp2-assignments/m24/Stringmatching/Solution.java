8f4160e24bd2003b989bbdc4536f7084bcdd1a8e
Abhilash11Addanki <abhilash11addanki@msitprogram.net>
2018-09-23 18:48:36 +0530

    "check_style_score -> 1 of 3 passed. style score: 0/1 "

--- cspp2-assignments/m24/Stringmatching/Solution.java
+++ cspp2-assignments/m24/Stringmatching/Solution.java
@@ -79,7 +79,11 @@ class Solution{
         for (int i = 0; i < length; i++) {
             System.out.print(filesList[i].getName() + "\t ");
             for (int j = 0; j < length; j++) {
-                System.out.print(matrix[i][j] + "        ");
+                if (matrix[i][j] == 100.0) {
+                    System.out.print(matrix[i][j] + "        ");
+                } else {
+                    System.out.print(matrix[i][j] + "         ");
+                }
             }
             System.out.println();
         }
